d6291fe7374fa64f31584e05a31c158313163585
Juliette Sinibardy <juju.siny@gmail.com>
2019-09-12 08:53:31 +0200

    :sparkles: Solve first challenge (1.1)

---
+++ challenges/1_easy_1/01.rb
@@ -0,0 +1,10 @@
+class Series
+  def initialize(numbers)
+    @numbers = numbers.split('').map(&:to_i)
+  end
+
+  def slices(count)
+    raise ArgumentError if count > @numbers.size
+    @numbers.each_cons(count).to_a
+  end
+end
