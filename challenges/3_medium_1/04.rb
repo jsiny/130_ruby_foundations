dc7cebbe8c36371b9363d7732074dcd331116f1d
Juliette Sinibardy <juju.siny@gmail.com>
2019-09-17 08:48:07 +0200

    :sparkles: Add challenge 3.4 (60 min)

---
+++ challenges/3_medium_1/04.rb
@@ -0,0 +1,32 @@
+class Translation
+  # rubocop:disable Layout/AlignHash
+  PROTEINS = {
+    'Methionine'    => ['AUG'],
+    'Phenylalanine' => ['UUU', 'UUC'],
+    'Leucine'       => ['UUA', 'UUG'],
+    'Serine'        => ['UCU', 'UCC', 'UCA', 'UCG'],
+    'Tyrosine'      => ['UAU', 'UAC'],
+    'Cysteine'      => ['UGU', 'UGC'],
+    'Tryptophan'    => ['UGG'],
+    'STOP'          => ['UAA', 'UAG', 'UGA']
+  }
+  # rubocop:enable Layout/AlignHash
+
+  def self.of_codon(codon)
+    PROTEINS.each do |protein, codons|
+      return protein if codons.include?(codon)
+    end
+    raise InvalidCodonError
+  end
+
+  def self.of_rna(strand)
+    strand.scan(/.../).each_with_object([]) do |codon, rna|
+      return rna if of_codon(codon) == 'STOP'
+      rna << of_codon(codon)
+    end
+  end
+end
+
+class InvalidCodonError < StandardError; end
+
+# Done in approx. an hour
