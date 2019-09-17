512598ec25d303a34a4d4822a0126209cacdcdb3
Juliette Sinibardy <juju.siny@gmail.com>
2019-09-17 10:13:49 +0200

    :sparkles: Add challenge 3.6 (25 min)

---
+++ challenges/3_medium_1/06.rb
@@ -0,0 +1,23 @@
+class School
+  attr_accessor :school
+
+  def initialize
+    @school = Hash.new { |name, grade| school[grade] = [] }
+  end
+
+  def to_h
+    school.keys.sort.each_with_object({}) do |num, clone|
+      clone[num] = grade(num)
+    end
+  end
+
+  def add(name, grade)
+    @school[grade] << name
+  end
+
+  def grade(num)
+    school[num].sort
+  end
+end
+
+# Done in 25 min
