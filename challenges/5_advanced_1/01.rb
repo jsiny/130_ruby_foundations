0e38b70931012cbb0f92476c3ee0a8e09edb5ae3
Juliette Sinibardy <juju.siny@gmail.com>
2020-01-14 10:06:16 +0100

    :sparkles: Add advanced challenge 5.1

---
+++ challenges/5_advanced_1/01.rb
@@ -0,0 +1,25 @@
+class OCR
+  @@digits = { [' _ ', '| |', '|_|'] => 0,    ['   ', '  |', '  |'] => 1,
+               [' _ ', ' _|', '|_ '] => 2,    [' _ ', ' _|', ' _|'] => 3,
+               ['   ', '|_|', '  |'] => 4,    [' _ ', '|_ ', ' _|'] => 5,
+               [' _ ', '|_ ', '|_|'] => 6,    [' _ ', '  |', '  |'] => 7,
+               [' _ ', '|_|', '|_|'] => 8,    [' _ ', '|_|', ' _|'] => 9 }
+
+  def initialize(str)
+    @str = str
+  end
+
+  def convert
+    @str.split("\n\n").map { |row| convert_row(row) }.join(',')
+  end
+
+  private
+
+  def convert_row(row)
+    rows = row.split("\n")
+    size = rows.max_by(&:size).size
+
+    split_rows = rows.map { |line| line.ljust(size).scan(/.../) }
+    split_rows.transpose.map { |num| @@digits.fetch(num, '?') }.join
+  end
+end
